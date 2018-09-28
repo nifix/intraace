ed939b0253de8f9b9f2e6c4fcfb31652f697bbb2
Nifix <nifixyn@gmail.com>
2018-09-28 22:01:48 +0200

    Added comments

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,26 @@
 
 Auth::routes();
 
-// Home ?
+/*
+*   Route for home page !
+*/
+
 Route::get('/', 'HomeController@index')->name('home');
 
-// Customers List Routes
+/*
+*   Routes for customers page.
+*/
 Route::get('customers/all', 'CustomersController@showAll')->name('cus.showAll');
 Route::get('customers/{id}', 'CustomersController@showCustomersByTeamId')->where('id','[\d]+')->name('cus.showByTeam');
 Route::get('customers/{id}/view', 'CustomersController@showCustomerById')->where('id','[\d]+')->name('cus.view');
 
-// Customers test :D
+/*
+*   Route to test ajax datatables.
+*/
 Route::get('export/data/customers', 'ExportDataController@test')->name('ajaxGetDataCustomers');
 
-// Customers Datatables - ajax request to return datatables.
+/*
+*   Routes used for ajax queries on the customers view.
+*/
 Route::get('datatables.data', 'ExportDataController@exportCustomers')->name('datatables.data');
 Route::get('datatables/{id}', 'ExportDataController@exportCustomersByTeamId')->name('datatables.databyid');
