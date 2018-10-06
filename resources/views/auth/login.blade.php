ed75a1fb099a7436ecf3e0300bf2f6ff2db9be67
Nifix <nifixyn@gmail.com>
2018-10-06 17:41:16 +0200

    Added timeline for dashboard, not yet dynamic, just a placeholder

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,11 +54,11 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('username') }}
+                                    {{ __('Se connecter') }}
                                 </button>
 
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
+                                    {{ __('Mot de passe oublié ?') }}
                                 </a>
                             </div>
                         </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,40 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="row">
+                        <div class="col-md-12 offset-md-0">
+                            <h4>Dernières annotations</h4>
+                            <ul class="timeline">
+                                <li>
+                                    <a target="_blank" href="#">RENAT</a>
+                                    <a href="#" class="float-right">Cédric le 08/10/2018</a>
+                                    <p>Estimation de la TVA d'Octobre à la demande du client. Le montant s'élève à 2 547€ (sans tenir compte
+                                        des différents taux de TVA des ventes).
+                                    </p>
+                                </li>
+                                <li>
+                                    <a href="#">MEDICA</a>
+                                    <a href="#" class="float-right">Emmanuelle le 07/10/2018</a>
+                                    <p>Liquidation en cours. Dossier comptable propre, vu avec clients. Ajout de la demande d'informations sur l'intranet</p>
+                                </li>
+                                <li>
+                                    <a href="#">Ze Kitchen</a>
+                                    <a href="#" class="float-right">Cédric le 07/10/2018</a>
+                                    <p>Saisie en attente à la demande de Pascale Bonnet.</p>
+                                </li>
+                                <li>
+                                    <a href="#">CCLM</a>
+                                    <a href="#" class="float-right">Cédric le 06/10/2018</a>
+                                    <p>Saisie à jour, demande d'informations effectuée le 04.10.18</p>
+                                </li>
+                            </ul>
                         </div>
-                    @endif
-
-                    You are logged in!
+                    </div>
                 </div>
             </div>
         </div>
